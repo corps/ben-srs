010736d0444afb02ac16418ff3014ae2f8f90eb8
Zachary Collins <zachary.collins@sentry.io>
2022-12-30 12:45:29 -0800

    Removing extension.

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,11 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 const stylesHandler = 'style-loader';
-const extPath = "BenSrsExt/Shared (Extension)/Resources"
 
 
 const config = {
     entry: {
         main: './src/index.tsx',
-        [path.join(extPath, "content")]: './src/ext/content.ts',
-        [path.join(extPath, "popup")]: './src/ext/popup.tsx',
-        [path.join(extPath, "background")]: './src/ext/background.ts',
     },
     output: {
         path: path.resolve(__dirname, 'docs'),
@@ -24,21 +20,6 @@ const config = {
             title: '弁SrS',
             chunks: ['main'],
             filename: 'index.html',
-        }),
-        new HtmlWebpackPlugin({
-            title: '弁SrS',
-            chunks: [path.join(extPath, "popup")],
-            filename: path.join(extPath, 'popup.html'),
-            inject: false,
-            templateContent: `<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>弁SrS</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1"></head>
-  <body>
-  <script src="popup.js"></script></body>
-</html>`
         }),
         new WorkboxPlugin.GenerateSW({
             // these options encourage the ServiceWorkers to get in there fast
